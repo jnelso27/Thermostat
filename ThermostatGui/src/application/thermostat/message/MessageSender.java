b82cf9c3ea9c6c3abb03a57d6de973a6b907a9fa
DeveloperMain <DeveloperMain@10.0.0.208>
2015-10-23 19:42:17 -0400

    - Added comments to the MessageSender class - Removed System.out debug message and replaced with Logger message

--- ThermostatGui/src/application/thermostat/message/MessageSender.java
+++ ThermostatGui/src/application/thermostat/message/MessageSender.java
@@ -1,12 +1,14 @@
 package application.thermostat.message;
 
+import application.thermostat.log.ThermostatLogger;
 import jssc.SerialPort;
 import jssc.SerialPortException;
 
 /***
- * Class Description
+ * Class which is used to send RS232 messages from a host platform (laptop)
+ * to the Teensy 3.1 microcontroller
  *
- * Date of Last Change: 2015-10-22
+ * Date of Last Change: 2015-10-23
  *
  * @author J Nelson
  *
@@ -25,27 +27,12 @@ public class MessageSender
 	}
 
 	/**
-	 * Constructor Description
-	 *
-	 * @param baud The baud rate to use for the serial communication
-	 * @param data_bits The number of data bits to use for the serial communication
-	 * @param stop_bits The number of stop bits to use for the serial communication
-	 * @param parity The number of parity bits to use
-	 */
-	public MessageSender(int baud, int data_bits, int stop_bits, int parity)
-	{
-		//Future Implementation
-	}
-
-	/**
 	 * Used to send a Message over the configured Serial Port
 	 *
-	 * @param messageToSend
+	 * @param messageToSend The message to send over the serial port to the Microcontroller
 	 */
 	public static void sendMessage(Message messageToSend)
 	{
-		System.out.println("Sending Message");
-
 		try
 		{
 			//Attempt to send the Message
@@ -53,7 +40,7 @@ public class MessageSender
 		}
 		catch(SerialPortException ex)
 		{
-			System.out.println("The Exception: " + ex + "Has Occurred");
+			ThermostatLogger.logger.severe("The Exception: " + ex + "Has Occurred");
 		}
 	}
 }
